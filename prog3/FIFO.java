3d03d59bda4ab9e5cb75a56adcf113ed87b12a00
jdafferner <james.dafferner@gmail.com>
2016-08-09 12:24:08 -0500

    FIFO
    Looks like the algorithm works in linux

--- prog3/FIFO.java
+++ prog3/FIFO.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class FIFO extends Algorithm {
 	
@@ -15,24 +16,34 @@ public class FIFO extends Algorithm {
 	@Override
 	public void compute() {
 		pageCount = 0;
-		if(pageTable.contains(references.get(inc))){
-			continue;
+		for(int inc = 0; inc < nr; inc++){
+		pageCount = 0;
+		for(int i = 0; i < nr; i++){
+			if(pageTable.contains(references.get(i))) { //hit
+				//System.out.printf("Hit on %d\n", references.get(i));
+				continue;
+			}
+			else if(pageCount < nf){ //empty frame available
+				pageTable.add(references.get(i));
+				//System.out.printf("Loading %d \n", references.get(i));
+				pageFault++;
+				pageCount++;
+			}
+			else{
+				//System.out.printf("Removing %d\n", pageTable.get(0));
+				pageTable.remove(0);
+				pageTable.add(references.get(i));
+				//System.out.printf("Added %d\n", pageTable.get(7));
+				pageFault++;
+				printPageTable();
+			}
 		}
-		else if(pageCount < nf){
-			pageTable.add(pageCount, references.get(inc));
-			pageCount++;
-			pageFault++;	
-		}
-		else{
-			
-		}
-		
-
+	double faultRate = (double) pageFault / (double) nr;
 	}
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		System.out.printf("\tFIFO: %d faults, fault rate = %.3f\n", pageFault, faultRate);
 
 	}
 
